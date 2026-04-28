87543bb032aed9e0e59fd2781d399153a6e597b7
hibiki2gou <e245742@ie.u-ryukyu.ac.jp>
2026-04-28 09:32:43 +0900

    regression.pyのpredict関数を実装

--- run_regression.py
+++ run_regression.py
@@ -4,12 +4,14 @@ import importlib
 importlib.reload(regression)
 
 X,Y = datasets.load_linear_example1()
+model = regression.LinearRegression()
+model.fit(X,Y)
 
 # ver.1
-model = regression.LinearRegression()
 print(model.x)
 
 # ver.2
-model = regression.LinearRegression()
-model.fit(X,Y)
 print(model.theta)
+
+# ver.3
+print(model.predict(X))
